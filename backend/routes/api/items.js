f9b885a991b498cea81c524851fc98ab6d1757b0
SAHIL SHARMA <76551267+KIET7UKE@users.noreply.github.com>
2022-09-08 20:02:26 +0530

    Fixed bugs

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -52,7 +52,9 @@ router.get("/", auth.optional, function(req, res, next) {
   if (typeof req.query.tag !== "undefined") {
     query.tagList = { $in: [req.query.tag] };
   }
-
+  if (typeof req.query.title !== "undefined") {
+    query.title = { $regex: req.query.title, $options: 'i' };
+  }
   Promise.all([
     req.query.seller ? User.findOne({ username: req.query.seller }) : null,
     req.query.favorited ? User.findOne({ username: req.query.favorited }) : null
