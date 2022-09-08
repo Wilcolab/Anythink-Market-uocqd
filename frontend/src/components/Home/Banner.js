84d8eea15dba49171ebdbbbbd29196622df1d536
SAHIL SHARMA <76551267+KIET7UKE@users.noreply.github.com>
2022-09-08 23:59:25 +0530

    Added search box

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,51 +1,23 @@
 import React from "react";
-import agent from "../../agent";
 import logo from "../../imgs/logo.png";
+import agent from "../../agent";
 
-const Banner = (props) => {
-  const changeTitle = (ev) => {
-    ev.preventDefault();
-    const title = ev.target.value;
-    if (title.length < 3) {
-      props.onTypeSearch(
-        title,
-        (page) => agent.Items.all(page),
-        agent.Items.all()
-      );
-      return;
-    }
-    props.onTypeSearch(
-      title,
-      (page) => agent.Items.byTitle(title, page),
-      agent.Items.byTitle(title)
-    );
-  };
+const Banner = () => {
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
           <span id="get-part">A place to get</span>
-          <div className="input-group mb-3" style={{ display: "initial" }}>
-            <input
-              id="search-box"
-              className="search-box"
-              type="text"
-              placeholder="Item Title"
-              onChange={changeTitle}
-            />
-            <div
-              className="input-group-append"
-              style={{ display: "inline-block" }}
-            >
-              <span
-                className="input-group-text"
-                style={{ background: null, display: "inline-block" }}
-              >
-                <i className="bi bi-search text-secondary"></i>
-              </span>
-            </div>
-          </div>
+          <input
+            id="search-box"
+            placeholder="What is it that you truly desire?"
+            onInput={(e) => {
+              const searchValue = e.target.value;
+              if (searchValue.length <= 3) return;
+              agent.Items.byTitle(searchValue);
+            }}
+          ></input>
           <span> the cool stuff.</span>
         </div>
       </div>
