10f697b8e45284a7d88bf3e9c01e848452c01651
SAHIL SHARMA <76551267+KIET7UKE@users.noreply.github.com>
2022-09-09 11:02:25 +0530

    Searchbox#3

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -52,6 +52,9 @@ router.get("/", auth.optional, function(req, res, next) {
   if (typeof req.query.tag !== "undefined") {
     query.tagList = { $in: [req.query.tag] };
   }
+  if  (typeof req.query.title !== "undefined") {
+    query.title =  { "$regex": req.query.title, "$options": "i" };
+  }
 
   Promise.all([
     req.query.seller ? User.findOne({ username: req.query.seller }) : null,
@@ -332,4 +335,5 @@ router.delete("/:item/comments/:comment", auth.required, function(
   }
 });
 
+module.exports = router;
 module.exports = router;
